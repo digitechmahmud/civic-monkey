337003c94b339fcf8791850dc67f5a568be84287
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2022-12-08 13:43:42 +0600

    Create About Section

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+            <div id='home' className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
                         <h1 className="mb-5 text-5xl font-bold">Civic Monkey Communication</h1>
                         <p className="mb-5">We’re passionate about building lifelong relationships on a foundation that starts with your marketing and business results.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-secondary">Get Started</button>
                     </div>
                 </div>
             </div>
--- src/pages/Home/Portfolio/Portfolio.js
+++ src/pages/Home/Portfolio/Portfolio.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const About = () => {
+const Portfolio = () => {
     return (
         <div>
-            <div id='about' className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+            <div id='portfolio' className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
                     <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-secondary text-">Get Started</button>
+                        <button className="btn btn-secondary">Get Started</button>
                     </div>
                 </div>
             </div>
@@ -17,4 +17,4 @@ const About = () => {
     );
 };
 
-export default About;
+export default Portfolio;
